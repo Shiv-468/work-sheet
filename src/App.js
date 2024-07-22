05b6c6aef2a5c35986833bbdb55d7343d2d6b449
Shiv-468 <shivmuwal.147@gmail.com>
2024-07-22 12:28:04 +0530

    change image from png to webp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import React,{useEffect, useState} from "react";
 import ProjectSheet from './components/ProjectSheet';
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const bodyStyle = {
+    width: windowWidth > 1920 ? '1920px' : '100%',
+    margin: windowWidth > 1920 ? '0 auto' : '0',
+  };
   return (
-    <>
+    <div className=' overflow-hidden' style={bodyStyle}>
     <ProjectSheet/>
-    </>
+    </div>
   )
 }
 
