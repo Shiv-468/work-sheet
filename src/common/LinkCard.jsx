6820067ffc82f93d8496850fcfab7640fb69c1d6
Shiv-468 <shivmuwal.147@gmail.com>
2024-07-24 09:28:53 +0530

    add new jsx for linkcard

---
+++ src/common/LinkCard.jsx
@@ -0,0 +1,19 @@
+import Images from './Images';
+export const Link_Card = ({githubLink, vercelLink, projectImage, i}) => (
+    <div key={i} className="flex flex-col md:flex-row w-full justify-between items-center border p-3 rounded-[16px] hover:shadow-LinkBox duration-300 ease-linear">
+    <div className="flex gap-1">
+      <h3 className="text-2xl font-medium font-Grandstander">{`${i + 1}.`}</h3>
+      <div className="flex flex-col">
+        <div className="flex flex-col md:flex-row md:gap-2">
+          <p className="font-Grandstander text-lg font-medium">GitHub Link:</p>
+          <a href={githubLink} target="_blank" rel="noopener noreferrer" className="text-base font-medium font-Poppins text-[#5928E5]">{githubLink}</a>
+        </div>
+        <div className="flex flex-col md:flex-row md:gap-2 mt-2">
+          <p className="font-Grandstander text-lg font-medium">Vercel Link:</p>
+          <a href={vercelLink} target="_blank"  className="text-base font-medium font-Poppins text-[#5928E5]" rel="noopener noreferrer">{vercelLink}</a>
+        </div>
+      </div>
+    </div>
+    <Images image={projectImage} className="max-w-[230px] max-h-[150px] rounded-[16px]" />
+  </div>
+)
--- src/components/FebruaryData.jsx
+++ src/components/FebruaryData.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import { februaryData } from '../common/Helper';
-import Images from '../common/Images';
-
+import { Link_Card } from '../common/LinkCard';
 const JanuaryData = ({ year }) => (
   <>
     {februaryData[year].map((project, index) => (
-      <div key={index} className="flex flex-col md:flex-row w-full justify-between items-center border p-3 rounded-[16px] hover:shadow-LinkBox duration-300 ease-linear">
-        <div className="flex gap-1">
-          <h3 className="text-2xl font-medium font-Grandstander">{`${index + 1}.`}</h3>
-          <div className="flex flex-col">
-            <div className="flex flex-col md:flex-row md:gap-2">
-              <p className="font-Grandstander text-lg font-medium">GitHub Link:</p>
-              <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="text-base font-medium font-Poppins text-[#5928E5]">{project.githubLink}</a>
-            </div>
-            <div className="flex flex-col md:flex-row md:gap-2 mt-2">
-              <p className="font-Grandstander text-lg font-medium">Vercel Link:</p>
-              <a href={project.vercelLink} target="_blank"  className="text-base font-medium font-Poppins text-[#5928E5]" rel="noopener noreferrer">{project.vercelLink}</a>
-            </div>
-          </div>
-        </div>
-        <Images image={project.image} className="max-w-[230px] max-h-[150px] rounded-[16px]" />
-      </div>
+       <Link_Card githubLink={project.githubLink} vercelLink={project.vercelLink} projectImage={project.image} i={index}/>
     ))}
   </>
 );
